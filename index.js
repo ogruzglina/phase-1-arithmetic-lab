6d9807241a2ab84ed79ddeb499546fda3f1bbe3d
Olha Gruzglina <olga.zhmura@gmail.com>
2022-01-09 22:22:54 -0500

    commit tree a944108506fceea90e695758d7e35e1d0619b047

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
+
+//let random = (Math.random() * 10 + 1).toFixed();
+//let random = parseInt(Math.random() * 10 + 1);
+//let random = Math.floor(Math.random() * 10 + 1); //round to lower value
+//let random = Math.round(Math.random() * 10 + 1); //if less than 5 then lower if more then higher
+//let random = Math.ceil(Math.random() * 10 + 1); //round to higher value
+let random = Math.trunc(Math.random() * 10 + 1); //cut everything after .
+console.log(random);
+
+let num3 = 24;
+let num4 = 5
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(4, -5, 13, 20, 3);
+console.log(max);
